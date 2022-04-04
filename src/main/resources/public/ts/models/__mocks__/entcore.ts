bd97944b622d1a8fcb6a3acc02b4825aa41c90f4
jinhda douang <s>
2022-04-04 11:33:23 +0200

    feat(properties): #DRIV-21 add properties workspace feat to nextcloud (#7)
    Co-authored-by: jinhda douang <s>
    feat(properties): #DRIV-21 add properties workspace feat to nextcloud (#7)

--- src/main/resources/public/ts/models/__mocks__/entcore.ts
+++ src/main/resources/public/ts/models/__mocks__/entcore.ts
@@ -43,3 +43,17 @@ export const ng = {
     directives: directives,
     services: services
 };
+
+export const model = {
+    calendar: {
+        dayForWeek: '2017-01-12T14:00:00.000+01:00'
+    },
+    me: {
+        userId: '7b6459f5-2765-45b5-8086-d5b3f422e69e',
+        type: 'PERSEDUCNAT',
+        hasWorkflow: jest.fn(() => true),
+        hasRight: jest.fn(() => true),
+        structures: [],
+        structureNames: []
+    },
+};
