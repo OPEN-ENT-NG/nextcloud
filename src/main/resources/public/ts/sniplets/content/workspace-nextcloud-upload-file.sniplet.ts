1a47c35b8e72744460a5889771709f9088e50d99
aymericdevriesere <101819152+aymericdevriesere@users.noreply.github.com>
2022-05-12 11:39:50 +0200

    feat(nextcloud): #DRIV-41 add front call to upload file to synchronized documents

--- src/main/resources/public/ts/sniplets/content/workspace-nextcloud-upload-file.sniplet.ts
+++ src/main/resources/public/ts/sniplets/content/workspace-nextcloud-upload-file.sniplet.ts
@@ -1,6 +1,7 @@
-import {Behaviours} from "entcore";
+import {Behaviours, model} from "entcore";
 import {NEXTCLOUD_APP} from "../../nextcloud.behaviours";
 import {safeApply} from "../../utils/safe-apply.utils";
+import {AxiosError} from "axios";
 
 declare let window: any;
 
@@ -56,8 +57,12 @@ export class UploadFileSnipletViewModel implements IViewModel {
     onValidImportFiles(): void {
         console.log("confirming uploading file: ", this.uploadedDocuments);
         // insert service tu upload file
-
-        Behaviours.applicationsBehaviours[NEXTCLOUD_APP].nextcloudService.sendOpenFolderDocument(this.vm.parentDocument);
+        this.vm.nextcloudService.uploadDocuments(model.me.userId, this.uploadedDocuments)
+            .then(() => Behaviours.applicationsBehaviours[NEXTCLOUD_APP].nextcloudService.sendOpenFolderDocument(this.vm.parentDocument))
+            .catch((err: AxiosError) => {
+                const message: string = "Error while attempting to rename document from content";
+                console.error(`${message}${err.message}: ${this.vm.getErrorMessage(err)}`);
+            })
         this.toggleUploadFilesView(false);
         safeApply(this.scope);
     }
