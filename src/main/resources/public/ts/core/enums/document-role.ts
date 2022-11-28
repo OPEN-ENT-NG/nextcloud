2df797604000a05ce9fdf1141c5cb96e82cc6c7d
aymericdevriesere <101819152+aymericdevriesere@users.noreply.github.com>
2022-11-28 13:51:10 +0100

    fix(thumbnail): #DRIV-68 display thumbnail for images is working (#81)

--- src/main/resources/public/ts/core/enums/document-role.ts
+++ src/main/resources/public/ts/core/enums/document-role.ts
@@ -2,7 +2,7 @@ export enum DocumentRole {
     XLS = 'spreadsheet',
     PPT = 'presentation',
     VIDEO = 'video',
-    IMG = 'img',
+    IMG = 'image',
     AUDIO = 'audio',
     DOC = 'doc',
     PDF = 'pdf',
