938e8626f457fd161d5db443edfc549a10f7fa4d
aymericdevriesere <101819152+aymericdevriesere@users.noreply.github.com>
2022-12-05 16:55:53 +0100

    fix(upload): #DRIV-66 import file with specials chars is now working (#86)
    fix(upload): #DRIV-66 import file with specials chars is now working

--- src/test/java/fr/openent/nextcloud/service/impl/DocumentServiceTest.java
+++ src/test/java/fr/openent/nextcloud/service/impl/DocumentServiceTest.java
@@ -2,13 +2,16 @@ package fr.openent.nextcloud.service.impl;
 
 import fr.openent.nextcloud.config.NextcloudConfig;
 import fr.openent.nextcloud.core.constants.Field;
-import fr.openent.nextcloud.helper.StringHelper;
+import fr.openent.nextcloud.helper.Attachment;
+import fr.openent.nextcloud.helper.Metadata;
 import fr.openent.nextcloud.model.UserNextcloud;
 import fr.openent.nextcloud.service.DocumentsService;
 import fr.openent.nextcloud.service.ServiceFactory;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.buffer.impl.BufferImpl;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
@@ -17,13 +20,21 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
 import org.entcore.common.sql.Sql;
+import org.entcore.common.storage.Storage;
+import org.entcore.common.storage.StorageFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
-@RunWith(VertxUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(VertxUnitRunner.class)
+@PrepareForTest({DefaultDocumentsService.class})
 public class DocumentServiceTest {
 
     private Vertx vertx;
@@ -31,6 +42,7 @@ public class DocumentServiceTest {
 
     private DocumentsService documentService;
     private ServiceFactory serviceFactory;
+    private Storage storage;
 
     @Before
     public void setUp() {
@@ -42,13 +54,56 @@ public class DocumentServiceTest {
                 .put(Field.ADMINCREDENTIAL, new JsonObject().put(Field.USERNAME, "test").put(Field.PASSWORD, "test"))
                 .put(Field.ENDPOINT, new JsonObject().put(Field.OCS_ENDPOINT_API, "ocs").put(Field.WEBDAV_ENDPOINT_API, "webdav"));
         NextcloudConfig nextcloudConfig = new NextcloudConfig(config);
-        serviceFactory = new ServiceFactory(vertx, null, null, null, null, webClient, nextcloudConfig);
-        this.documentService = Mockito.spy(new DefaultDocumentsService(serviceFactory));
+        this.storage = Mockito.spy(new StorageFactory(vertx, null).getStorage());
+        serviceFactory = new ServiceFactory(vertx, this.storage, null, null, null, webClient, nextcloudConfig);
+        PowerMockito.spy(DefaultDocumentsService.class);
+        this.documentService = PowerMockito.spy(new DefaultDocumentsService(serviceFactory));
         Sql.getInstance().init(vertx.eventBus(), "fr.openent.next");
     }
 
     @Test
-    public void testListFiles(TestContext ctx) {
+    public void testUploadFile(TestContext ctx) throws Exception {
+        Async async = ctx.async();
+        String filePath = "Documents/test % test";
+        String encodedFilePath = "Documents/test%20%25%20test";
+        String userId = "1";
+        String userName = "test";
+        String userToken = "token";
+        String expectedURI = serviceFactory.nextcloudConfig().host() + serviceFactory.nextcloudConfig().webdavEndpoint() + "/" + userId + "/" + encodedFilePath;
+
+
+        UserNextcloud.TokenProvider userSession = new UserNextcloud.TokenProvider();
+        userSession.setUserId(userId);
+        userSession.setUserName(userName);
+        userSession.setToken(userToken);
+
+        HttpRequest<Buffer> httpRequest = Mockito.spy(HttpRequest.class);
+        PowerMockito.doReturn(Future.succeededFuture(filePath)).when(this.documentService, "getUniqueFileName", Mockito.any(), Mockito.anyString(), Mockito.eq(0));
+        Mockito.doAnswer(invocation -> {
+            Handler<Buffer> var2 = invocation.getArgument(1);
+            var2.handle(new BufferImpl());
+            return null;
+        }).when(this.storage).readFile(Mockito.anyString(), Mockito.any());
+
+        Mockito.doAnswer(invocation -> {
+            String absoluteURI = invocation.getArgument(0);
+            ctx.assertEquals(absoluteURI, expectedURI);
+            async.complete();
+            return httpRequest;
+        }).when(this.webClient).putAbs(Mockito.anyString());
+
+        Mockito.doReturn(httpRequest).when(httpRequest).basicAuthentication(Mockito.anyString(), Mockito.anyString());
+        Mockito.doReturn(httpRequest).when(httpRequest).as(Mockito.any());
+
+        Mockito.doNothing().when(httpRequest).sendBuffer(Mockito.any(), Mockito.any());
+        Mockito.doNothing().when(this.storage).removeFile(Mockito.anyString(), Mockito.any());
+        this.documentService.uploadFile(userSession, new Attachment("id", new Metadata(new JsonObject().put(Field.FILENAMELOWER, "filename"))), null);
+
+        async.awaitSuccess(10000);
+    }
+
+    @Test
+    public void testListFiles(TestContext ctx) throws Exception {
         Async async = ctx.async();
         String filePath = "Documents/test % test";
         String encodedFilePath = "Documents/test%20%25%20test";
@@ -75,7 +130,9 @@ public class DocumentServiceTest {
 
         Mockito.doReturn(httpRequest).when(httpRequest).basicAuthentication(Mockito.anyString(), Mockito.anyString());
         Mockito.doReturn(httpRequest).when(httpRequest).as(Mockito.any());
+        PowerMockito.doReturn("").when(documentService, "getListFilesPropsBody");
         Mockito.doNothing().when(httpRequest).sendBuffer(Mockito.any(), Mockito.any());
+
         this.documentService.parameterizedListFiles(userSession, filePath, null);
 
         async.awaitSuccess(10000);
@@ -132,13 +189,6 @@ public class DocumentServiceTest {
         this.documentService.moveDocument(userSession, filePath, moveURI);
 
         async.awaitSuccess(5000);
-    }
-
-    @Test
-    public void testEncode(TestContext ctx) {
-        String encodedURL = "test à accent";
-        String decodedURL = StringHelper.encodeUrlForNc(encodedURL);
-        ctx.assertEquals(decodedURL, "test%20%C3%A0%20accent");
     }
 
 }
