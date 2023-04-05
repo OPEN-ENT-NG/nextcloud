0137e5436a686245f9656d48a7f6a0d91087ae4f
BOES <gregoryboes33@gmail.com>
2023-04-05 12:04:43 +0200

    feat(directive): #DRIV-100 fit progress bar quota value to nextcloud server user storage space

--- src/main/resources/public/ts/models/__test__/nextcloud-user.model.test.ts
+++ src/main/resources/public/ts/models/__test__/nextcloud-user.model.test.ts
@@ -4,16 +4,11 @@ describe('NextcloudModel', () => {
 
     it('test Quota.build with big used', () => {
         const quota = new Quota().build({
-            free: 805306368,
             used: 1342177280,
             total: 2147483648,
-            relative: 12.42,
-            quota: 2147483648,
         })
 
-        expect(quota.free).toEqual(805306368);
-        expect(quota.quota).toEqual(2147483648);
-        expect(quota.relative).toEqual(12.42);
+
         expect(quota.total).toEqual(2);
         expect(quota.unit).toEqual('Go');
         expect(quota.used).toEqual(1.25);
@@ -21,16 +16,10 @@ describe('NextcloudModel', () => {
 
     it('test Quota.build with small used', () => {
         const quota = new Quota().build({
-            free: 2061584303,
             used: 85899345,
             total: 2147483648,
-            relative: 12.42,
-            quota: 2147483648,
         })
 
-        expect(quota.free).toEqual(2061584303);
-        expect(quota.quota).toEqual(2147483648);
-        expect(quota.relative).toEqual(12.42);
         expect(quota.total).toEqual(2);
         expect(quota.unit).toEqual('Go');
         expect(quota.used).toEqual(0.08);
@@ -38,16 +27,11 @@ describe('NextcloudModel', () => {
 
     it('test Quota.build with less than 2000 Mo', () => {
         const quota = new Quota().build({
-            free: 410300000,
             used: 1200312736,
             total: 1610612736,
-            relative: 12.42,
-            quota: 1610612736,
         })
 
-        expect(quota.free).toEqual(410300000);
-        expect(quota.quota).toEqual(1610612736);
-        expect(quota.relative).toEqual(12.42);
+
         expect(quota.total).toEqual(1536);
         expect(quota.unit).toEqual('Mo');
         expect(quota.used).toEqual(1145);
