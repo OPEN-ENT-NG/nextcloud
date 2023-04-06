07c78ceda09daef1264b7e34475320fec03b2f77
Gregprod33 <72068635+Gregprod33@users.noreply.github.com>
2023-04-06 12:22:44 +0200

    feat(directive): #DRIV-100 fit progress bar quota value to nextcloud server user storage space (#100)
    * feat(directive): #DRIV-100 fit progress bar quota value to nextcloud server user storage space
    
    * fix(tests): #DRIV-100 adapt tests with new values of nextcloud-user.model
    
    * feat(directive): #DRIV-100 add i18n tag for Espace Illimité information

--- src/main/resources/public/ts/models/nextcloud-user.model.ts
+++ src/main/resources/public/ts/models/nextcloud-user.model.ts
@@ -33,19 +33,14 @@ export class UserNextcloud {
 }
 
 export class Quota {
-    free: string;
     used: number;
     total: number;
-    relative: number;
-    quota: number;
     unit: string;
 
+
     build(data: any): Quota {
-        this.free = data.free;
         this.used = data.used / (1024 * 1024);
-        this.total = data.total / (1024 * 1024);
-        this.relative = data.relative;
-        this.quota = data.quota;
+        this.total = data.quota / (1024 * 1024);
         if (this.total > 2000) {
             this.total = Math.round((this.total / 1024) * 100) / 100;
             this.used = Math.round((this.used / 1024) * 100) / 100;
