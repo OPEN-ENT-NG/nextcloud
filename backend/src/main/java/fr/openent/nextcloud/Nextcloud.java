65fea51b5beff3481afcf8a36db56f02297d00bf
Alex Delaveau <57497688+alex-delaveau@users.noreply.github.com>
2024-05-17 09:53:09 +0200

    feat(migration): #DRIV-117 migration Vertx 4 (#104)

--- backend/src/main/java/fr/openent/nextcloud/Nextcloud.java
+++ backend/src/main/java/fr/openent/nextcloud/Nextcloud.java
@@ -7,6 +7,7 @@ import fr.openent.nextcloud.controller.UserController;
 import fr.openent.nextcloud.controller.NextcloudDesktopController;
 import fr.openent.nextcloud.service.ServiceFactory;
 import fr.wseduc.mongodb.MongoDb;
+import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
@@ -25,8 +26,8 @@ public class Nextcloud extends BaseServer {
 	public static final String DB_SCHEMA = "nextcloud";
 
 	@Override
-	public void start() throws Exception {
-		super.start();
+	public void start(Promise<Void> startPromise) throws Exception {
+		super.start(startPromise);
 		final Map<String, NextcloudConfig> nextcloudConfigMapByHost = new HashMap<>();
 		if (config.containsKey("nextcloud-providers")) {
 			final JsonObject nexcloudProviders = config.getJsonObject("nextcloud-providers", new JsonObject());
@@ -50,6 +51,9 @@ public class Nextcloud extends BaseServer {
 		addController(new UserController(serviceFactory));
 		addController(new DocumentsController(serviceFactory));
 		addController(new NextcloudDesktopController(serviceFactory));
+
+        startPromise.tryComplete();
+		startPromise.tryFail("[NextCloud@NextCloud::start] Fail to start NextCloud");
 	}
 
 	private WebClient initWebClient() {
--- backend/src/main/java/fr/openent/nextcloud/helper/FutureHelper.java
+++ backend/src/main/java/fr/openent/nextcloud/helper/FutureHelper.java
@@ -2,20 +2,20 @@ package fr.openent.nextcloud.helper;
 
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
-import io.vertx.core.impl.CompositeFutureImpl;
 
 import java.util.List;
 
 public class FutureHelper {
     public static <T> CompositeFuture all(List<Future<T>> futures) {
-        return CompositeFutureImpl.all(futures.toArray(new Future[futures.size()]));
+
+        return Future.all(futures);
     }
 
     public static <T> CompositeFuture join(List<Future<T>> futures) {
-        return CompositeFutureImpl.join(futures.toArray(new Future[futures.size()]));
+        return Future.join(futures);
     }
 
     public static <T> CompositeFuture any(List<Future<T>> futures) {
-        return CompositeFutureImpl.any(futures.toArray(new Future[0]));
+        return Future.any(futures);
     }
 }
