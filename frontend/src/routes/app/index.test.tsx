bbafd735fae2df849ddfbaca38cc006b20d3265d
Nikolaï AMOSSE <nikolai.93@outlook.com>
2024-09-18 12:27:15 +0200

    fix: #DRIV-140 fix pipeline test

--- frontend/src/routes/app/index.test.tsx
+++ frontend/src/routes/app/index.test.tsx
@@ -1,15 +1,28 @@
+import { ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { describe, it } from "vitest";
 
+import { GlobalProvider } from "~/providers/GlobalProvider";
 import { App } from "~/routes/app/index.tsx";
+import theme from "~/styles/theme";
+
+
+const queryClient = new QueryClient();
 
 describe("App Test", () => {
-  it("Rendering app page should render", () => {
+  it("should render the App component", () => {
     render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
+      <QueryClientProvider client={queryClient}>
+        <GlobalProvider>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </ThemeProvider>
+        </GlobalProvider>
+      </QueryClientProvider>,
     );
   });
 });
